81ce0f075432f500d6e3c01637a8f9557f6d6a1c
manikGit <srivastavamanik2@gmail.com>
2019-06-04 16:13:02 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CardsReOrder from './components/CardsReOrder';
+import CardsReOrder from './components/CardsReOrder'; //without react-beautiful-dnd
 import CardsAccessible from './components/CardsAccessible';
 
 ReactDOM.render(<CardsAccessible />, document.querySelector('#root'))
